bae7c0b88defb225791861830e60372e804df76f
@wwwjim <send@jimmy.email>
2020-07-31 11:59:00 -0700

    0.0.7 - makes example more pure

--- example-api/pages/index.js
+++ example-api/pages/index.js
@@ -1,17 +1,4 @@
 import * as React from "react";
-import * as System from "slate-react-system";
-
-import { css } from "@emotion/react";
-
-const STYLES_FILE_HIDDEN = css`
-  height: 1px;
-  width: 1px;
-  opacity: 0;
-  visibility: hidden;
-  position: fixed;
-  top: -1px;
-  left: -1px;
-`;
 
 export default class SlateIndexPage extends React.Component {
   _handleUpload = async (e) => {
@@ -22,6 +9,8 @@ export default class SlateIndexPage extends React.Component {
     let data = new FormData();
 
     data.append("image", file);
+
+    console.log("Making a storage request");
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -31,33 +20,15 @@ export default class SlateIndexPage extends React.Component {
     });
 
     const json = await response.json();
+    console.log({ json });
   };
 
   render() {
     return (
       <div>
-        <System.H1>You will need your own API keys for this to work.</System.H1>
-        <br />
-        <br />
-        <System.P>
-          <br />
-          <br />
-          <div style={{ marginTop: 24 }}>
-            <input
-              css={STYLES_FILE_HIDDEN}
-              type="file"
-              id="file"
-              onChange={this._handleUpload}
-            />
-            <System.ButtonPrimary
-              style={{ margin: "0 16px 16px 0" }}
-              type="label"
-              htmlFor="file"
-            >
-              Upload File
-            </System.ButtonPrimary>
-          </div>
-        </System.P>
+        <h1>You will need your own API keys for this to work.</h1>
+
+        <input type="file" onChange={this._handleUpload} />
       </div>
     );
   }
